953879e67c554070cb25225af86d68deed820c92
Knol <k@knolaust.com>
2024-02-13 16:23:21 -0600

    Modify to reflect new build process.

--- aos-blocks.php
+++ aos-blocks.php
@@ -8,9 +8,8 @@
  * Author URI:  https://knolaust.com
  */
 function aos_blocks_enqueue_assets() {
-    wp_enqueue_style('aos-style', plugins_url('node_modules/aos/dist/aos.css', __FILE__));
-    wp_enqueue_script('aos-script', plugins_url('node_modules/aos/dist/aos.js', __FILE__), array(), '1.0.0', true);
-    wp_enqueue_script('aos-blocks-script', plugins_url('assets/js/aos-blocks.js', __FILE__), array('aos-script'), '1.0.0', true);
+    wp_enqueue_style('aos-blocks-style', plugins_url('assets/css/aos-blocks.css', __FILE__));
+    wp_enqueue_script('aos-blocks-script', plugins_url('assets/js/aos-blocks.bundle.js', __FILE__), array(), '1.0.0', true);
     wp_add_inline_script('aos-script', 'AOS.init();');
 }
 add_action('wp_enqueue_scripts', 'aos_blocks_enqueue_assets'); 
